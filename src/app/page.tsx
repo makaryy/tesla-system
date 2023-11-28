5842aaca87aef1e12819330177a595ba3b6b1d7c
Makary Pagacz <pagaczmakary@gmail.com>
2023-11-28 23:13:21 +0100

    hero and first 2 sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,67 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <>
+            <section className="flex flex-col lg:flex-row px-4 py-6 lg:py-12 justify-between items-center gap-4 lg:gap-8 lg:min-h-screen">
+                <div className="p-3 flex-[1_1_50%]">
+                    <h2 className="text-3xl xl:text-4xl font-bold min-w-xs pb-6">Chroń swoją własność dzięki niezawodnym systemom alarmowym</h2>
+                    <article className="space-y-3 flex-1 min-w-xs ">
+                        <p className="xl:text-lg pb-3">
+                            Rozumiemy znaczenie zapewnienia bezpieczeństwa Twojej własności. Dlatego oferujemy najwyższej klasy systemy alarmowe,
+                            które zapewniają całodobową ochronę.
+                        </p>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                        <div className="flex flex-col lg:flex-row gap-3">
+                            <div>
+                                <h3 className="pb-2 text-lg font-semibold">Rozszerzona ochrona</h3>
+                                <p className="pb-3">
+                                    Nasze systemy alarmowe zapewniają kompleksową ochronę przed włamaniem, włamaniem i nieuprawnionym dostępem.
+                                </p>
+                            </div>
+                            <div>
+                                <h3 className="pb-2 text-lg font-semibold">Całodobowa ochrona</h3>
+                                <p className="pb-3">
+                                    Nasz zespół ekspertów zapewnia całodobowy nadzór i natychmiastową reakcję na wszelkie zagrożenia bezpieczeństwa.
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                <div className="flex-[1_1_50%] mt-3">
+                    <Image className="aspect-square w-full object-cover" src="/cables.jpg" alt="" width={1440} height={810} />
+                </div>
+            </section>
+            <section className="flex flex-col lg:flex-row px-4 py-6 justify-between items-center gap-4 lg:gap-8 lg:min-h-screen">
+                <div className="p-3 flex-[1_1_50%]">
+                    <h2 className="text-3xl xl:text-4xl font-bold min-w-xs pb-6">Zaawansowane systemy CCTV do kompleksowego nadzoru</h2>
+                    <article className="space-y-3 flex-1 min-w-xs ">
+                        <p className="text-lg pb-3">
+                            Nasza oferta systemów CCTV oferuje najnowocześniejsze funkcje i zaawansowaną technologię, aby zapewnić kompleksowy nadzór
+                            i bezpieczeństwo. Dzięki kamerom o wysokiej rozdzielczości, detekcji ruchu i możliwościom zdalnego dostępu możesz
+                            monitorować swój obiekt z łatwością i spokojem.
+                        </p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                        <ul className="flex flex-col gap-2 list-disc list-inside text-lg">
+                            <li>Kamery o wysokiej rozdzielczości zapewniają krystalicznie czysty materiał</li>
+                            <li>Wykrywanie ruchu, aby powiadomić Cię o wszelkich podejrzanych działaniach</li>
+                            <li>Zdalny dostęp do monitorowania obiektu z dowolnego miejsca</li>
+                        </ul>
+                        <div className="flex gap-4 my-6">
+                            <button className="border-2 border-text-100 px-4 py-2 text-text-100 capitalize text-sm hover:shadow-md transition-shadow">
+                                Więcej informacji
+                            </button>
+                            <button className="flex items-center gap-2 px-4 py-2 capitalize text-sm text-text-100 hover:shadow-md transition-shadow">
+                                <span>Zgłoś się</span> <FaArrowRightLong />
+                            </button>
+                        </div>
+                    </article>
+                </div>
+                <div className="flex-[1_1_50%] mt-3 lg:-order-1">
+                    <Image className="aspect-square w-full object-cover" src="/cables.jpg" alt="" width={1440} height={810} />
+                </div>
+            </section>
+        </>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            maxWidth: {
+                "8xl": "1440px"
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+            },
+            colors: {
+                primary: {
+                    100: "#c21d03",
+                    200: "#fd5732",
+                    300: "#ffb787"
+                },
+                accent: {
+                    100: "#393939",
+                    200: "#bebebe"
+                },
+                text: {
+                    100: "#232121",
+                    200: "#4b4848"
+                },
+                bg: {
+                    100: "#fbfbfb",
+                    200: "#f1f1f1",
+                    300: "#c8c8c8"
+                }
+            }
+        }
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: []
+};
+export default config;
