1739ed0ef05fa93af4d8d6b45285ee0ff8d52a60
Makary Pagacz <pagaczmakary@gmail.com>
2023-12-11 13:00:40 +0100

    style: form error animation

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -3,7 +3,10 @@ import { ContactForm } from "./ContactForm";
 const Contact = () => {
     return (
         <div className="flex-grow p-4 lg:p-8 mt-16 self-center w-full max-w-lg mx-auto">
-            <ContactForm />
+            <div className="mt-8">
+                <h1 className="text-4xl font-bold mb-8 text-center">Skontaktuj się</h1>
+                <ContactForm />
+            </div>
         </div>
     );
 };
