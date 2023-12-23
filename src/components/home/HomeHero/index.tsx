0e5f8f732667482ce4a6e4fed13e93b1a661ed4f
Makary Pagacz <pagaczmakary@gmail.com>
2023-12-23 16:05:23 +0100

    fix: change homepage hero image

--- src/components/home/HomeHero/index.tsx
+++ src/components/home/HomeHero/index.tsx
@@ -6,12 +6,13 @@ const subtitle =
     "Specjalizujemy się w dostarczaniu najlepszej jakości rozwiązań ochrony i bezpieczeństwa Twojej własności i ukochanych. naszymi najnowocześniejszymi systemami i doświadczonym zespołem możesz mieć pewność, że jesteś w dorych rękach.";
 const primaryCta = { href: "/oferta", label: "Oferta" };
 const secondaryCta = { href: "/kontakt", label: "Skontaktuj się" };
+const image = { src: "https://res.cloudinary.com/dqyhe8ogi/image/upload/v1703342229/tesla-system/hero_f8wyjq.jpg", alt: "Cameras on a wall" };
 
 const HomeHero = () => {
     return (
         <>
             <div className="absolute -z-10 top-0 w-full h-screen">
-                <Image src="/hero.jpg" alt="Hero Image" className="object-cover object-left" fill priority />
+                <Image src={image.src} alt={image.alt} className="object-cover object-left" fill priority />
             </div>
             <section className="relative flex gap-6 px-7 lg:px-20 pb-6 pt-20 justify-between items-center min-h-screen w-full bg-black/50">
                 <div className="relative space-y-4 max-w-7xl mx-auto">
